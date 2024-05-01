6a8a75f2750ffd59217d7eb63c02245af093fd34
vamsy2112 <vamsy.2112@gmail.com>
2024-05-02 01:25:48 +0530

    dataTypes in cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //int
+    int y;
+    cin >> y;
+    cout << "value of y is:" << y;
+
+    //long
+    long x;
+    cin >> x;
+    cout << "value of x in long is: " << x;
+    return 0;
+
+    // long long
+    long long z = 1222222222;
+    cout << "value of z in long long is: " << z;
+
+    //float 
+
+    float a =2.50054;
+    cout << a;
+
+    //double
+    double b = 1.97352739;
+    cout << b;
+
+    //string
+    string x1, x2;
+    cin >> x1 >> x2;
+    cout << x1 << " " << x2;
+
+    //getline
+    string c;
+    getline(cin, c);
+    cout << c;
+
+    // char
+
+    char ch = 'h';
+    cout << ch;
+
+}
